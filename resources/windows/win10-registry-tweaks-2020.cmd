9bdb31d43f5fc9cf1fa313dbc0c242c29e3d702d
Emyll Almonte <eg0dz@aol.com>
2024-10-16 23:07:52 -0400

    Update win10-registry-tweaks-2020.cmd

--- resources/windows/win10-registry-tweaks-2020.cmd
+++ resources/windows/win10-registry-tweaks-2020.cmd
@@ -1,2 +1,4 @@
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer /v DisableNotificationCenter /t REG_DWORD /d 1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0
+
+REM Need to add services like data usage , delivery optimization, tabletinputservice, etc.. in Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\...
